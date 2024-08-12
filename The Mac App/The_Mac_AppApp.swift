76f649e07c4523ece49fb9e61e411e873e130679
paulr <paulyd0123@gmail.com>
2024-08-12 15:02:54 -0400

    Initial Commit

---
+++ The Mac App/The_Mac_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Mac_AppApp.swift
+//  The Mac App
+//
+//  Created by Pauly Rodriguez on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct The_Mac_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
