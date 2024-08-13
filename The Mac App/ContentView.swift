64063dae15babe182db34e03af825bcdab841764
paulr <paulyd0123@gmail.com>
2024-08-13 10:17:43 -0400

    added more images assets to the main screen. Also made the images into buttons. Working on creating new views.

--- The Mac App/ContentView.swift
+++ The Mac App/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    func buttonPressed () {
+        print("button pressed")
+    }
+    
+    let makImages = [
+        #imageLiteral(resourceName: "mak1"), #imageLiteral(resourceName: "mak2"), #imageLiteral(resourceName: "mak3"), #imageLiteral(resourceName: "mak4")
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    VStack {
+                        Button(action: {
+                            buttonPressed()
+                        }) {
+                            Image("mak1")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 350, height: 350)
+                                .cornerRadius(20)
+                        }
+                        
+                        Text("Makayla Cheyenne Rodriguez")
+                            .font(.title3)
+                        
+                        //image2
+                        Button(action: {
+                            buttonPressed()
+                        }) {
+                            Image("mak2")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 350, height: 350)
+                                .cornerRadius(20)
+                        }
+                        Text("The Goofy Mak")
+                            .font(.title3)
+                        
+                        //image3
+                        Button(action: {
+                            buttonPressed()
+                        }){
+                            Image("mak3")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 350, height: 350)
+                                .cornerRadius(20)
+                        }
+                        Text("Beauty Mak")
+                    }
+                    
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
